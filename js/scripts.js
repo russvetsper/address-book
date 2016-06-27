cbbb9e93f044d50a59df240cd91127a5e754bd02
russ vetsper and sid benavente <russ+sid@>
2016-06-27 10:55:16 -0700

    add last name method to contact object

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-console.log("hello");
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// use rinterface logic.... build a user interface and display.
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    // console.log(inputtedFirstName + inputtedLastName);
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    // console.log(newContact);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+      console.log("string")
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+  });
+});
